f146d5142704ab1503c6f4d7fa439faaf7bb86c1
João Bispo <joaobispo2077@gmail.com>
2022-01-29 16:11:49 -0300

    feat: add custom hook useTranslation

---
+++ src/i18n/index.ts
@@ -0,0 +1,13 @@
+import * as enUS from './enUS';
+import * as ptBR from './ptBR';
+
+export type i18nTranslation = Record<string, string>;
+
+export type i18nTranslationsObject = Record<keyof typeof ptBR, i18nTranslation>;
+
+export type I18n = Record<string, i18nTranslationsObject>;
+
+export const i18n: I18n = {
+  'en-us': enUS,
+  'pt-br': ptBR,
+};
