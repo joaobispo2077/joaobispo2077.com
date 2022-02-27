62599d37a0ead03cf756954cb9d15ff18bf720ed
João Bispo <joaobispo2077@gmail.com>
2022-02-27 17:43:18 -0300

    feat: setup codegen with urql and graphql

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,8 @@
-const { GITHUB_ACCESS_TOKEN } = process.env;
+const { GITHUB_ACCESS_TOKEN, NEXT_PUBLIC_GITHUB_URL } = process.env;
 
 export const auth = {
   github_api: {
     accessToken: String(GITHUB_ACCESS_TOKEN),
+    url: String(NEXT_PUBLIC_GITHUB_URL),
   },
 };
