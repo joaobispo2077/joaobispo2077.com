c5d07a74e25ca6f55f420cec0dcefa06b5fe21ba
João Bispo <joaobispo2077@gmail.com>
2022-04-09 14:01:23 -0300

    feat(wip): add posts and post detail page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,8 @@ import { Provider as UrqlProvider } from 'urql';
 
 import { serverSideCache } from '@src/services/ServerSideCache';
 import { GithubClient } from '@src/services/GithubClient';
+import { Header } from '@src/components/Header';
+import { Footer } from '@src/components/Footer';
 
 function App({ Component, pageProps }: AppProps) {
   if (pageProps.urqlState) {
@@ -13,7 +15,9 @@ function App({ Component, pageProps }: AppProps) {
 
   return (
     <UrqlProvider value={GithubClient}>
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </UrqlProvider>
   );
 }
