daad1bbbb373a5db80a9fdd40a2ca0806bbd2115
João Bispo <joaobispo2077@gmail.com>
2022-04-09 14:48:41 -0300

    feat: add styles into Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,49 @@
 import { FunctionComponent } from 'react';
 
+import {
+  Box,
+  Heading,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+
 export const Header: FunctionComponent = () => {
   return (
-    <header>
-      <h1>Header</h1>
-    </header>
+    <Box
+      as="header"
+      height="5rem"
+      width="100%"
+      padding="1rem"
+      display="flex"
+      justifyContent={'space-between'}
+      backgroundColor="brand.background"
+    >
+      <Heading as="h1" fontSize="2rem" fontWeight="bold" color="brand.primary">
+        JB
+      </Heading>
+
+      <Menu>
+        {({ isOpen }) => (
+          <>
+            <MenuButton
+              as={IconButton}
+              aria-label="Options"
+              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            />
+            <MenuList>
+              <MenuItem>Sobre</MenuItem>
+              <MenuItem>Artigos</MenuItem>
+              <MenuItem>Repositórios</MenuItem>
+              <MenuItem>Projetos</MenuItem>
+              <MenuItem>Flow</MenuItem>
+            </MenuList>
+          </>
+        )}
+      </Menu>
+    </Box>
   );
 };
