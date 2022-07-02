2eda74eccd8779f678724ece0a9164b516d7855e
João Bispo <joaobispo2077@gmail.com>
2022-07-02 12:20:36 -0300

    feat: parse next locale to graph cms locale

--- src/hooks/useTranslation.ts
+++ src/hooks/useTranslation.ts
@@ -1,13 +1,20 @@
 import { useRouter } from 'next/router';
 
+import { Locale } from '@src/generated/graphql.blog';
+import { parseLocaleToGraphCmsLocale } from '@src/utils/parseLocale';
+
 import { i18n, i18nTranslationsObject } from '../i18n';
 
 type UseTranslationsReturn = i18nTranslationsObject & {
   locale: string;
+  graphCmsLocale: Locale;
 };
+
 export const useTranslation = (): UseTranslationsReturn => {
   const { locale = 'en-us' } = useRouter();
 
+  const graphCmsLocale = parseLocaleToGraphCmsLocale(locale);
+
   const pagesText = i18n[locale as keyof typeof i18n];
-  return { ...pagesText, locale: locale };
+  return { ...pagesText, locale: locale, graphCmsLocale };
 };
