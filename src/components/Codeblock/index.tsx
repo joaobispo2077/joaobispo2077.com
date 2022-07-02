476cb14469db31f7cfa9b94e377ff5640e350af5
João Bispo <joaobispo2077@gmail.com>
2022-07-02 12:54:30 -0300

    fix: import as cjs instead esm

--- src/components/Codeblock/index.tsx
+++ src/components/Codeblock/index.tsx
@@ -2,7 +2,7 @@ import { FunctionComponent } from 'react';
 
 import { Box } from '@chakra-ui/react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { dracula } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 
 export type CodeblockProps = {
   code: string;
