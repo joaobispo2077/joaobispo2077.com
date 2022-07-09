c688b0b65efae24a3d1b27b8128d35f21238d9e5
João Bispo <joaobispo2077@gmail.com>
2022-07-09 15:19:19 -0300

    refactor: add custom options into formatDate functions

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,11 +1,13 @@
 export function formatDate(
   date: Date,
   locale: string | string[] | undefined = 'en-us',
+  options: Intl.DateTimeFormatOptions = {},
 ): string {
   const formattedDate = new Intl.DateTimeFormat(locale, {
     year: 'numeric',
     month: 'short',
     day: '2-digit',
+    ...options,
   }).format(new Date(date));
   return formattedDate;
 }
