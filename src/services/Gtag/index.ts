38df04a48bc489df75da9d25b03f25772baa7408
João Bispo <joaobispo2077@gmail.com>
2022-07-17 16:00:22 -0300

    refactor: remove unused export

--- src/services/Gtag/index.ts
+++ src/services/Gtag/index.ts
@@ -7,15 +7,13 @@ type EventProps<T> = {
   value: T;
 };
 
-export const pageview = (url: string) => {
-  console.debug('pageview', url);
-  console.debug('pagapis.analytics.trackingIdeview', apis.analytics.trackingId);
+const pageview = (url: string) => {
   window.gtag('config', apis.analytics.trackingId, {
     page_path: url,
   });
 };
 
-export const event = <T>({ action, category, label, value }: EventProps<T>) => {
+const event = <T>({ action, category, label, value }: EventProps<T>) => {
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
