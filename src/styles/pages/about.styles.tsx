5eb978d759603683e6a8beea4b2509a92a714be0
João Bispo <joaobispo2077@gmail.com>
2022-07-17 17:30:48 -0300

    feat: add about description styles

---
+++ src/styles/pages/about.styles.tsx
@@ -0,0 +1,11 @@
+export const AboutDescriptionStyles = {
+  strong: {
+    color: 'brand.primary',
+  },
+  p: {
+    marginBottom: '1.5rem',
+    '&:last-child': {
+      marginBottom: 0,
+    },
+  },
+};
