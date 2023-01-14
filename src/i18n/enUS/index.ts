c3b47cfdfe1c39277e890e879ab9e96ec3e0676e
João Bispo <joaobispo2077@gmail.com>
2023-01-14 13:24:45 -0300

    feat: add fallback translation for roadmap page

--- src/i18n/enUS/index.ts
+++ src/i18n/enUS/index.ts
@@ -28,3 +28,12 @@ export const aboutTranslation = {
   fallbackContent:
     '<p>Hey! I&#39;m <strong>João Bispo</strong>, I started as a software developer in 2015, trying to rotate a bull on my screen writing my first code lines with JavaScript.<br></p><p>I&#39;m currently <strong>Software Engineer</strong> at IBM. Since my start, I really like solving problems with software craftship using concepts of software engineering, algorithms efficiency and choose the right tool for the job. I&#39;m originally from <strong>Brazil</strong>, I want to know other countries and cities, but I&#39;ve always lived here.<br></p><p>In my spare time I like to foster my creativite, doing <strong>PixelArt</strong>, write articles, watch anime and play games. I also like to workout and <strong>chocolate milkshake</strong>.</p>',
 };
+
+export const roadmapTranslation = {
+  title: 'Higher. Further. Faster.',
+  description:
+    'This is my roadmap, I will update it as I learn new things and improve my skills.',
+  seoTitle: 'Roadmap',
+  seoDescription:
+    'This is my roadmap, I will update it as I learn new things and improve my skills.',
+};
