359c699f12fcb054ccaab9f54c673cb481b43c15
João Bispo <joaobispo2077@gmail.com>
2023-07-20 18:51:24 -0300

    feat: add feedbacks data structure

---
+++ src/data/feedbacks.ts
@@ -0,0 +1,104 @@
+export const feedbacks = [
+  {
+    name: "Luiz Gustavo Chinelato Setten",
+    avatar: "https://avatars.githubusercontent.com/u/13947201?v=4",
+    comment: `
+    João é uma pessoa muito competente, proativa, dedicada e comprometida, nos projetos que nós atuamos juntos sempre pude notar essa competência tanto em ensinar quanto de articular equipes, liderar pessoas e resolver problemas. Pude aprender muito com você sobre AWS e também como lidar com pessoas.
+    Peço que continue fazendo o excelente trabalho e continue estudando, as tecnologias mudam e sempre precisamos acompanhá-las. Te reconheço como um exemplo a ser seguido, tanto como colega, líder e mentor.
+`,
+    date: "May 19 2023"
+  },
+  {
+    name: "Lucas Juan Souza de Oliveira Miranda Moraes",
+    avatar: "https://avatars.githubusercontent.com/u/13947201?v=4",
+    comment: `
+    João,
+
+    Eu honestamente não nem palavras curtas pra descrever o grande profissional que você é e vem se tornando e demonstrando durante esse tempo, portanto tentarei ser conciso.
+    Desde que começamos a nos falar, eu já tinha percebido diversas das suas características de ser centrado e responsável, o que tem sido altamente apreciado, acredito que não só por mim, mas como todo mundo da cadeia e da squad como um todo.
+
+    Você tem uma mentalidade centrada e madura, demonstra um compromisso notável em produzir um trabalho de alta qualidade, se dedica a entender os requisitos de cada demanda ou necessidade e busca soluções eficientes, sempre mantendo o foco nos objetivos e resultados de cada novidade que a gente coloca na ponta do lápis. A sua sagacidade e busca rápida por subterfúgios(problem solving) com o pé no chão a partir de meros indícios é um diferencial raríssimo e digno de reconhecimento de qualquer um.
+
+    Ademais, a sua responsabilidade com cada compromisso firmado (TogToAWS, ritos da squad e muitos outros) é exemplar.
+    E uma das coisas que mais admiro em pessoas e que isso inclui você é o fato de que está sempre disposto a compartilhar conhecimentos e ajudar os colegas, mostrando um espírito de colaboração e trabalho em equipe que deixaria qualquer pessoa sem palavras. Por fim, posso dizer que, quem te conhece facilmente cria uma confiança profissional gigantesca em cima de ti. Agradeço imensamente pela oportunidade de me colocar em seu ciclo profissional e por ter me ensinado e guiado tão bem a partir da sua experiência.
+`,
+    date: "May 19 2023"
+  },
+  {
+    name: "Vicente Augusto Kolinoski Magalhaes",
+    avatar: "https://avatars.githubusercontent.com/u/13947201?v=4",
+    comment: `
+    João possui um potencial incrível. Embora não trabalhe diretamente com ele, posso afirmar que, em termos de Hardskills, ele é um profissional exemplar. Sempre entrega seus projetos dentro do prazo e cumpre seus compromissos com os clientes. Já ouvi diversas recomendações positivas de pessoas próximas.
+    No que diz respeito às Softskills e outras qualidades, concordo plenamente que João é uma pessoa altamente comunicativa. Ele se expressa com clareza e está sempre disposto a ajudar quem precisa. Além disso, sua presença na equipe fullstack é muito valorizada.
+`,
+    date: "May 19 2023"
+  },
+  {
+    name: "Adriano Matuck Sotos",
+    avatar: "https://avatars.githubusercontent.com/u/13947201?v=4",
+    comment: `
+    Fala João,
+
+    Gostaria de iniciar esse feedback te parabenizando pelos esforços e influência positiva que tem feito entre os colegas que atuam no BB. Até o momento não tivemos nenhuma experiência técnica para eu poder te avaliar nesse quesito, mas a partir das suas falas, perguntas e  sugestões, consigo deduzir uma maturidade no assunto.
+
+    Agora mais relacionado as atividades que atuamos junto, destaco sua pro atividade e empatia com os demais colegas. Vejo que você se preocupa com o bem estar e crescimento dos demais, isso é muito massa, e vou te falar que essas atitudes têm me ajudado no dia a dia. Consigo enxergar skills de liderança em você, então como dica, eu investiria também nessa área que você poderá ter destaque.
+
+    O ponto de melhoria que eu consegui visualizar e gostaria de compartilhar com você é a resposta à frustrações. Não que seja algo que te impacte negativamente (não que eu tenha vivenciado), mas noto que você fica chateado e desanimado quando coisas que deveriam ser de um jeito (que acreditamos ser o certo), não são, porém são coisas que fogem do nosso controle e alçada e não podemos fazer nada (para que elas sejam do jeito que a gente quer). Então bora canalizar essa energia para fazer o que a gente defende que uma hora ou outra os frutos nascem. Você é agilizado e enquanto mantiver essa postura proativa, empática e humanista, não perderá seu valor.
+
+    Obrigado pela parceria
+
+    Abraços
+`,
+    date: "May 19 2023"
+  },
+  {
+    name: "Michael Medina da Cunha",
+    avatar: "https://avatars.githubusercontent.com/u/13947201?v=4",
+    comment: `
+    O João é um dos melhores profissionais que conheci, solícito, seu team play é um dos diferenciais, mas irei dividir essa resposta nos tópicos, hardskill e softskill como solicitado.
+    HardSkill
+    Sobre as skills técnicas, é um profissional com uma desenvoltura muito grande, pois, ele sabe mesclar seu conhecimento com uma determinada linguagem de programação, com as boas maneiras de se programar, por exemplo, fazendo uso de design patterns e conceitos de código limpo, tornando assim seu projeto, legível e manutenível. Além do mais o João é um profissional que sempre busca se manter atualizado.
+
+    SoftSkill
+
+    Nesse um ano que me encontro na IBM, consegui reparar em uma evolução grande por parte do João, em relação a softskills, ele sempre busca aplicar os conceitos de IBM Garage, tem foco no time, respeita os companheiros, pensa "fora da caixa", e essa é uma característica imprescindível para criarmos soluções disruptivas, porém, a evolução que se destaca mais em minha opinião é o story telling, o João consegue apresentar muito bem, conduzir reuniões e improvisar quando algo não sai como o esperado, colocando as apresentações nos "trilhos" novamente.
+`,
+    date: "May 19 2023"
+  },
+  {
+    name: "Stephanie Saggal",
+    avatar: "https://avatars.githubusercontent.com/u/13947201?v=4",
+    comment: `
+    João você é um profissional excepcional, nunca tive problemas em trabalhar com você e além de tudo sempre está disposto a tocar novas ideias e auxiliar no que for preciso para elas acontecerem. A iniciativa de AWS ta sensacional e ajudando várias pessoas a tirar a certificação e também obrigada pelo auxílio na guilda de typescript!
+`,
+    date: "May 19 2023"
+  },
+  {
+    name: "Giovanna Albuquerque Riello",
+    avatar: "https://avatars.githubusercontent.com/u/13947201?v=4",
+    comment: `
+    Oi João. Há algum tempo estamos trabalhando juntos na cadeia de apoio da Squad Full Stack 1, e tenho visto de perto o sua dedicação, comprometimento e profissionalismo com os colegas e a IBM.
+
+    Tenho visto que é uma pessoa bastante proativa e comunicativa. Além disso, possui um forte perfil de liderança, sendo um ótimo mestre de cerimônia nos ritos da Squad Vertical (Weekly, Guilda e Monthly), e está sempre contribuindo com ideias valiosas nas iniciativas e nas discussões.
+
+    Vejo também que é um ótimo palestrante. Estive presente na sua Guilda sobre Clean Code e posso afirmar que você transmitiu um conteúdo de alta qualidade para os colegas. A apresentação e o material que você preparou estavam excelentes.
+
+    Também gostaria de citar, e te parabenizar pela iniciativa “togtoaws”. Sem duvidas, ela a ponte que muitos colegas precisavam para conquistar a certificação AWS Cloud Practitioner. Parabéns por todo empenho e dedicação envolvidos.
+
+    Eu acredito que você inspira e influencia positivamente os colegas. Continue com o seu excelente trabalho. :)
+`,
+    date: "Jun 22 2023"
+  },
+  {
+    name: "Tulio Fernando Nogueira",
+    avatar: "https://avatars.githubusercontent.com/u/13947201?v=4",
+    comment: `
+    Olá João, estando atuando no mesmo cliente e também na mesma squad vertical eu gostaria de pontuar as seguintes coisas:
+    - Tem sido proativo nas atividades, como uma das pessoas a frente do grupo de estudos para tirar a certificação AWS.
+    - Nas reuniões da squad também tem se botado a frente, puxando tópicos importantes do momento.
+    - Durante as guildas da squad o fato de você dar uma ajuda pro apresentador, sendo quebrando o gelo, como na minha apresentação ou  ajudando a expandir a discussão do assunto, como na da Sthepanie, eu julgo ter sido um ponto muito positivo.
+    Então de maneira geral, tem sido bastante cooperativo para com o Projeto, IBM e colegas, parabéns pelo bom trabalho.
+`,
+    date: "May 22 2023"
+  },
+]
