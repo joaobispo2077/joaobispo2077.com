00fcd82f9235195fbe85eea934aaf2a1493694a2
João Bispo <joaobispo2077@gmail.com>
2025-04-24 00:31:03 -0300

    refactor: replace first page image by a self-hosted image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
     >
       <SEO
         description="Crafting software for the web"
-        image="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80"
+        image="https://us-west-2.graphassets.com/A2rVan5iKQwq5RAG15Bnqz/cm9usucbotdlo0dn1ks5vgv5x"
         url="/"
       />
 
