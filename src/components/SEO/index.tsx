b4ed25ad19c863120e3a6f50c66d39938de06ed8
João Bispo <joaobispo2077@gmail.com>
2025-04-24 09:07:37 -0300

    refactor: remove itemProp="image" for adding dupicated information w/o adding any new semantics

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -50,7 +50,7 @@ export const SEO: FunctionComponent<SEOProps> = ({
       <meta property="og:url" content={canonicalUrl} />
       <meta property="og:title" content={fullTitle} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" itemProp="image" content={image} />
+      <meta property="og:image" content={image} />
       {/* <meta property="og:image:secure_url" itemProp="image" content={image} /> */}
       <meta property="og:image:type" content="image/jpeg" />
       <meta property="og:image:width" content="1200" />
