d05e34e0169cd7fe663fa35d4c0ddbeb6a18cb58
João Bispo <joaobispo2077@gmail.com>
2026-04-16 18:30:40 -0300

    chore(cypress): drop component testing, keep E2E only

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,17 +2,9 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.ts',
-  },
-  component: {
-    devServer: {
-      framework: 'next',
-      bundler: 'webpack',
-    },
-    specPattern: 'cypress/component/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/component.ts',
   },
 });
 
